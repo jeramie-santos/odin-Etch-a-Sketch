2b8352cf5cb1c709e904fcfd16ce025536739637
Jeramie-Santos <jeramieramsantos79@gmail.com>
2024-02-11 16:41:10 +0800

    mouse over color

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 A Etch a Sketch project in The Odin Project.
 - JS DOM
 - HTML
+- CSS
--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 let mainContainer = document.querySelector('#main-container');
 
-
-
-
 for (let x = 1; x <= 16; x++){    
     let container = document.createElement('div');
     container.setAttribute('id', 'container');
     for (let i = 1; i <= 16; i++){
         let div = document.createElement('div');
-        div.setAttribute('id', `item`);
+        div.setAttribute('class', `item`);
         container.appendChild(div);
         console.log(i);
     }
     mainContainer.appendChild(container);
 }
+
+let items = document.querySelectorAll('.item');
+
+for (let i = 0; i <= items.length; i++){
+    items[i].addEventListener('mouseover', function(){
+        items[i].style.cssText = "background: gray";
+    });
+}
+
+
+
 
