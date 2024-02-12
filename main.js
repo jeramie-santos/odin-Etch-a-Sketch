91f24db8865892ebe8fe0c74b9497cc4d6448fef
Jeramie-Santos <jeramieramsantos79@gmail.com>
2024-02-12 09:58:09 +0800

    random color on mouseover

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 let mainContainer = document.querySelector('#main-container');
-let canva = document.querySelector('#canva-size');
+let canvas = document.querySelector('#canvas-size');
 let btn = document.querySelector('#btn-square');
+
+const RGB_RED = '255, 0, 0';
+const RGB_ORANGE = '255, 160, 16';
+const RGB_YELLOW = '255, 224, 32';
+const RGB_GREEN = '0, 192, 0';
+const RGB_BLUE = '0, 32, 255';
+const RGB_PURPLE = '160, 32, 255';
+const RGB_BLACK = '0, 0, 0';
+const RGB_GREY = '128, 128, 128';
+
+
 
 btn.addEventListener('click', createCanvas);
 
@@ -16,7 +27,16 @@ for (let x = 1; x <= 16; x++){
     mainContainer.appendChild(container);
 }
 
-canva.textContent = `Canvas size 16 x 16`;
+let items = document.querySelectorAll('.item');
+
+for (let i = 0; i <= items.length; i++){
+    items[i].addEventListener('mouseover', function(){
+        items[i].style.cssText = `background: rgb(${getRgbColor()})`;
+    });
+}
+
+canvas.textContent = `Canvas size 16 x 16`;
+
 
 
 function createCanvas(){
@@ -54,28 +74,41 @@ function createCanvas(){
         }
     }
 
-    canva.textContent = `Canvas size ${numberOfSquares} x ${numberOfSquares}`;
+    canvas.textContent = `Canvas size ${numberOfSquares} x ${numberOfSquares}`;
 
     let items = document.querySelectorAll('.item');
 
     for (let i = 0; i <= items.length; i++){
         items[i].addEventListener('mouseover', function(){
-            items[i].style.cssText = "background: gray";
+            items[i].style.cssText = `background: rgb(${getRgbColor()})`;
         });
     }
+
     
 }
 
 
-let items = document.querySelectorAll('.item');
+function getRgbColor(){
+    let rgbColor;
+    let num = Math.floor((Math.random() * 8) + 1);
+    if (num == 1){
+        rgbColor = RGB_BLUE;
+    } else if (num == 2){
+        rgbColor = RGB_ORANGE;
+    } else if (num == 3){
+        rgbColor = RGB_YELLOW;
+    }else if (num == 4){
+        rgbColor = RGB_GREEN;
+    } else if (num == 5){
+        rgbColor = RGB_BLUE;
+    } else if (num == 6){
+        rgbColor = RGB_PURPLE;
+    } else if (num == 7){
+        rgbColor = RGB_BLACK;
+    } else if (num == 8){
+        rgbColor = RGB_GREY;
+    }
 
-for (let i = 0; i <= items.length; i++){
-    items[i].addEventListener('mouseover', function(){
-        items[i].style.cssText = "background: gray";
-    });
+    return rgbColor;
 }
-
-
-
-
 
